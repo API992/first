88e635f29447521b0676b9af8a54dd57772e7d1f
API992 <78959670+API992@users.noreply.github.com>
2021-02-12 18:13:06 +0530

    updated evenodd

--- evenodd.java
+++ evenodd.java
@@ -6,6 +6,7 @@ public class Evenodd {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter number to check whether odd or even");
 		a=sc.nextInt();
+		System.out.println("From github");
 		if(a % 2==0)
 		{
 			System.out.println("Even");
