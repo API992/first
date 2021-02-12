f0d3c845bcf8e6ead50c45159d9ffc7ad6ee87f4
gitrepos <tejas.bora@apisero.com>
2021-02-12 18:08:58 +0530

    evenodd program

---
+++ evenodd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Evenodd {
+
+	public static void main(String[] args) {
+		int a;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number to check whether odd or even");
+		a=sc.nextInt();
+		if(a % 2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
